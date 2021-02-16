2a3740e5810bb72fb70d16cced64b13ce844ee36
SangMokChae <72491390+SangMokChae@users.noreply.github.com>
2021-02-16 21:09:36 +0900

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hi");
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
+// listen이 실행하라고 명령하는 방식이다.
