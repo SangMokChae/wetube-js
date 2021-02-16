6f2fee4a0e0007239c5ae0a058139997f574d650
SangMokChae <72491390+SangMokChae@users.noreply.github.com>
2021-02-16 22:11:30 +0900

    use the middleware

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const express = require('express');
+import "core-js";
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-const handleListening = () => {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
 // req = request Object, res = response Object
 // res.send 는 req받은 것을 화면으로 보내주는 것이다.
-const handleHome = (req, res) => {
-  res.send('Hello from HOME!!');
-};
+const handleHome = (req, res) => res.send('Hello from ME!!');
 
-const handleProfile = (req, res) => {
-  res.send("You are on my profile");
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+// middleware
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next(); // 다음꺼를 호출
 }
 
 // routes
+app.use(betweenHome);
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
