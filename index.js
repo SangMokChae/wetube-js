6742f369601097482754537be912bae4d9819f82
SangMokChae <72491390+SangMokChae@users.noreply.github.com>
2021-02-16 21:24:05 +0900

    made a basic server

--- index.js
+++ index.js
@@ -7,5 +7,20 @@ const handleListening = () => {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
 
+// req = request Object, res = response Object
+// res.send 는 req받은 것을 화면으로 보내주는 것이다.
+const handleHome = (req, res) => {
+  res.send('Hello from HOME!!');
+};
+
+const handleProfile = (req, res) => {
+  res.send("You are on my profile");
+}
+
+// routes
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
 // listen이 실행하라고 명령하는 방식이다.
