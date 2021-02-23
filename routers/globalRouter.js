07b4afc08224cff5394d120ffec0fad8b31edc37
SangMokChae <72491390+SangMokChae@users.noreply.github.com>
2021-02-23 21:45:31 +0900

    join get/post and join form connect

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { join, login, logout } from "../controllers/userController";
+import { getJoin, postJoin, login, logout } from "../controllers/userController";
 import { home, search } from "../controllers/videoController";
 import routes from "../routes"; // ..밖의 자료위치
 const globalRouter = express.Router();
 
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
+
 globalRouter.get(routes.home, home); // controller에 export를 해줘야 사용할 수 있는 기능이다.
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 
