c4caff7473e8a38e9a4c02c6280bc20e93532fca
SangMokChae <72491390+SangMokChae@users.noreply.github.com>
2021-02-24 21:43:04 +0900

    used env and hidden the important key

--- init.js
+++ init.js
@@ -1,7 +1,10 @@
 import "./db";
 import app from "./app";
+import dotenv from 'dotenv';
+dotenv.config();
+// env를 사용해서 오픈하고 싶지 않은 키를 숨길 수 있다.
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => 
   console.log(`✔ Listening on: http://localhost:${PORT}`);
