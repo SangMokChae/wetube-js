9b384bbe30e80ac75738f07b3b08b47bae263d44
SangMokChae <72491390+SangMokChae@users.noreply.github.com>
2021-02-26 00:04:12 +0900

    changed videoDetail

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -33,7 +33,18 @@ export const postUpload = async(req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail"});
+export const videoDetail = async(req, res) => {
+  const {
+    params: {id}
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    // findById 는 mongoose의 query옵션중에 하나이다.
+    res.render("videoDetail", { pageTitle: "Video Detail", video}); // video변수를 템플릿에 전달
+  } catch(error) {
+    res.redirect(routes.home);
+  }
+}
 
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video"});
 
