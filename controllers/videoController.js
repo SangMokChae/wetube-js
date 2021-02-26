dd246ca3448034bccb1287869dfbbbaba364a24c
SangMokChae <72491390+SangMokChae@users.noreply.github.com>
2021-02-26 22:39:20 +0900

    add comment fix Searching and regular expression

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -14,8 +14,16 @@ export const home = async(req, res) => {
 // async는 javascript에게 함수의 어떤 부분을 꼭 기다리게 만드는 것이다.(왜냐하면 javascript는 한번에 여러 작업이 가능하기 때문에 넘어가지 않게 하기 위한 작업이라고 보면 된다.)
 // await는 다음 과정이 끝날때까지 잠시 기다려 달라고 요청하는 것이다. (await는 async없이 사용할 수 없다.)
 
-export const search = (req, res) => {
+export const search = async(req, res) => {
   const { query: { term: searchingBy } } = req;
+  let videos = [];
+  try { // 비디오를 찾은 경우
+    videos = await Video.find({ title: { $regex : searchingBy, $options: "i" }});
+    // 만약 Video.find({title: searchingBy })로 입력한다면 내가 search input에 입력한 똑같은 것만 찾을 것이다. 
+    // 단어를 포함한 것을 찾기 위해서 mongoose의 regex를 이용한다. // $options: "i"=(insensitive: 덜민감한) -> 대소문자를 구분하지 않게 하기 위해서 추가하는 옵션이다.
+  } catch(error) {
+    console.log(error);
+  }
   res.render("search", { pageTitle: "Search", searchingBy, videos });
 } ;
 
