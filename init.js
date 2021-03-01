7aaf4cbc0ca3ec1aacbd9d8947c7cf0d667dc0cd
SangMokChae <72491390+SangMokChae@users.noreply.github.com>
2021-03-01 13:08:22 +0900

    passport-local and later i can login by other site ID

--- init.js
+++ init.js
@@ -1,10 +1,13 @@
+import dotenv from 'dotenv';
 import "./db";
 import app from "./app";
-import dotenv from 'dotenv';
+
 dotenv.config();
 // env를 사용해서 오픈하고 싶지 않은 키를 숨길 수 있다.
+
 import "./models/Video";
 import "./models/Comment";
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
---
+++ passport.js
@@ -0,0 +1,5 @@
+import passport from "passport";
+import User from "./models/User";
+
+passport.use()
+// strategey: 로그인 방식 / 원하는 만큼 사용가능 (facebook strategy, github strategy)
